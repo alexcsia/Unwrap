a2a0db9ea55c1f34ae16d7d7b3171725ce4fdd8e
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-03-01 22:31:11 +0100

    create config folder for passport

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,31 @@
-import express from 'express';
-import path from 'path';
-import './controllers/auth.controller';
-import authRoutes from './routes/auth.routes';
-import generalRoutes from './routes/general.routes';
-import historyRoutes from './routes/history.routes';
-import expressSession from 'express-session';
+import express from "express";
+import path from "path";
+import "./config/passport";
+import authRoutes from "./routes/auth.routes";
+import generalRoutes from "./routes/general.routes";
+import historyRoutes from "./routes/history.routes";
+import expressSession from "express-session";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(expressSession({
-  secret: process.env.SESSION_SECRET || "Better to be safe than sorry",
-  resave: false,
-  saveUninitialized: false,
-}))
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET || "Better to be safe than sorry",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
-app.use(historyRoutes)
+app.use(historyRoutes);
 app.use(authRoutes);
-app.use(generalRoutes)
+app.use(generalRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ src/config/passport.ts
@@ -0,0 +1,47 @@
+import passport from "passport";
+import { Strategy as SpotifyStrategy } from "passport-spotify";
+import prisma from "../utils/prisma.util";
+import { saveUser } from "../models/user.model";
+
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      callbackURL: process.env.SPOTIFY_CALLBACK_URI!,
+    },
+    async (accessToken, refreshToken, expires_in, profile, done) => {
+      try {
+        const spotifyId = profile.id as string;
+
+        let user = await saveUser(
+          spotifyId,
+          accessToken,
+          refreshToken,
+          profile,
+        );
+
+        return done(null, user);
+      } catch (error) {
+        console.error("Error during authentication:", error);
+        return done(error as Error, undefined);
+      }
+    },
+  ),
+);
+
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: id as string },
+    });
+    done(null, user);
+  } catch (error) {
+    console.error("Error during deserialization:", error);
+    done(error, null);
+  }
+});
