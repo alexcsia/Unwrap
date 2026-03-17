aa7d401756aa258876d9c41eeb2084a80a776173
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-03-17 14:33:19 +0100

    add typeguard for error messages

--- src/errors/ApiError.ts
+++ src/errors/ApiError.ts
@@ -1,7 +1,9 @@
+import type { ErrorCode } from "./error-messages";
+
 export class ApiError extends Error {
   statusCode: number;
-  code: string; // A short internal code
-  constructor(statusCode: number, code: string, message?: string) {
+  code: ErrorCode; // A short internal code
+  constructor(statusCode: number, code: ErrorCode, message?: string) {
     super(message);
     this.statusCode = statusCode;
     this.code = code;
---
+++ src/errors/error-messages.ts
@@ -0,0 +1,13 @@
+export const ERROR_MESSAGES = {
+  INVALID_UPLOAD: "Uploaded file or folder is not valid",
+  NO_FILES_FOUND: "Please select a file to upload",
+  DB_ERROR: "Failed to save listening history",
+  UNAUTHORIZED: "You are not authorized",
+  UNAUTHENTICATED: "Authentication failed",
+  DEFAULT: "Internal server error",
+  MISSING_PLATFORM: "Platform parameter is missing",
+  UNSUPPORTED_PLATFORM: "Unsupported platform",
+  SPOTIFY_API_ERROR: "Error while fetching Spotify API",
+} as const;
+
+export type ErrorCode = keyof typeof ERROR_MESSAGES;
