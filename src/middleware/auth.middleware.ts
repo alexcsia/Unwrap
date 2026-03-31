183612417025d4542566574cb8bb8fbbe42f1835
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-03-31 13:11:41 +0200

    add auth bypass for dev mode

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,14 +1,31 @@
+import jwt from "jsonwebtoken";
 import { ApiError } from "@/errors/ApiError";
 import type { Request, Response, NextFunction } from "express";
 
 export const checkAuth = async (
   req: Request,
   res: Response,
-  _next: NextFunction,
+  next: NextFunction,
 ) => {
-  if (req.isAuthenticated()) {
-    return _next();
-  } else {
-    throw new ApiError(401, "UNAUTHORIZED", "Failed auth check");
+  console.log("Current ENV:", process.env.NODE_ENV);
+
+  if (process.env.NODE_ENV === "development") {
+    req.user = { id: "1234" };
+    return next();
+  }
+
+  const accessToken = req.cookies.accessToken;
+  if (!accessToken) {
+    return next(new ApiError(401, "UNAUTHORIZED", "Missing access token"));
+  }
+
+  try {
+    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET!) as {
+      userId: string;
+    };
+    req.user = { id: decoded.userId };
+    next();
+  } catch (error) {
+    next(new ApiError(401, "UNAUTHORIZED", "Invalid or expired token"));
   }
 };
