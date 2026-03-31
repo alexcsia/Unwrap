5556d3ab24a9bef1424405b98c35941e3b97c14d
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-03-31 13:51:22 +0200

    add CORS config

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,25 @@ import "@/config/passport";
 import authRoutes from "./routes/auth.routes";
 import generalRoutes from "@/routes/general.routes";
 import historyRoutes from "@/routes/history.routes";
-import expressSession from "express-session";
+import topTracksRoutes from "@/routes/top-tracks.routes";
 import { errorMiddleware } from "@/middleware/error.middleware";
 import passport from "passport";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Cookie"],
+  }),
+);
 
 if (
   process.env.NODE_ENV === "development" &&
@@ -24,21 +37,15 @@ if (
     });
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  expressSession({
-    secret: process.env.SESSION_SECRET || "Better to be safe than sorry",
-    resave: false,
-    saveUninitialized: false,
-  }),
-);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(authRoutes);
 app.use(historyRoutes);
 app.use(generalRoutes);
+app.use(topTracksRoutes);
 
 app.use(errorMiddleware);
 
