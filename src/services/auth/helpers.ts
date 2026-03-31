5f68a0a48dad97393319e807289042b2372ad994
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-03-31 13:14:21 +0200

    create auth endpoint and service

---
+++ src/services/auth/helpers.ts
@@ -0,0 +1,34 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET || "supersecret_access";
+
+export const bcryptHash = async (varToHash: string): Promise<string> => {
+  const hashed = await bcrypt.hash(varToHash, 10);
+
+  return hashed;
+};
+
+export const comparePasswords = async (
+  receivedPassword: string,
+  userPassword: string,
+): Promise<boolean> => {
+  const passwordMatch = await bcrypt.compare(
+    await bcryptHash(receivedPassword),
+    userPassword,
+  );
+  return passwordMatch;
+};
+
+export const generateTokens = async (
+  userId: string,
+): Promise<{ accessToken: string; refreshToken: string }> => {
+  const accessToken = jwt.sign({ sub: userId }, ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+
+  const refreshToken = crypto.randomBytes(32).toString("hex");
+
+  return { accessToken, refreshToken };
+};
