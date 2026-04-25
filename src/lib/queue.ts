0f143f181f9f8e3a4a4fbacdc8ec602506dfee89
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-04-25 18:18:27 +0200

    fix redis port issue

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -1,24 +1,12 @@
 import { Queue, type ConnectionOptions } from "bullmq";
-import IORedis, { type RedisOptions } from "ioredis";
+import IORedis from "ioredis";
 
-const REDIS_URL = process.env.REDIS_URL;
-
-export const redisConnection = (
-  REDIS_URL
-    ? REDIS_URL
-    : {
-        host: process.env.REDIS_HOST || "localhost",
-        port: Number(process.env.REDIS_PORT) || 6379,
-      }
-) as ConnectionOptions;
-
+const REDIS_URL = process.env.REDIS_URL!;
+export const redisConnection = REDIS_URL as ConnectionOptions;
 export const historyQueue = new Queue("history-sync", {
-  connection: redisConnection,
-  defaultJobOptions: {
-    removeOnComplete: true,
+  connection: {
+    url: REDIS_URL,
   },
 });
 
-export const redisCache = REDIS_URL
-  ? new IORedis(REDIS_URL, { maxRetriesPerRequest: null })
-  : new IORedis(redisConnection as RedisOptions);
+export const redisCache = new IORedis(REDIS_URL);
