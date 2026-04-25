81f8db87f4bb59fa8fc7a1f70a9179688072cdb3
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-04-25 17:30:32 +0200

    add redis url env var

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -1,14 +1,24 @@
-import { Queue } from "bullmq";
-import IORedis from "ioredis";
+import { Queue, type ConnectionOptions } from "bullmq";
+import IORedis, { type RedisOptions } from "ioredis";
 
-export const redisConnection = {
-  host: process.env.REDIS_HOST || "localhost",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  maxRetriesPerRequest: null,
-};
+const REDIS_URL = process.env.REDIS_URL;
+
+export const redisConnection = (
+  REDIS_URL
+    ? REDIS_URL
+    : {
+        host: process.env.REDIS_HOST || "localhost",
+        port: Number(process.env.REDIS_PORT) || 6379,
+      }
+) as ConnectionOptions;
 
 export const historyQueue = new Queue("history-sync", {
   connection: redisConnection,
+  defaultJobOptions: {
+    removeOnComplete: true,
+  },
 });
 
-export const redisCache = new IORedis(redisConnection);
+export const redisCache = REDIS_URL
+  ? new IORedis(REDIS_URL, { maxRetriesPerRequest: null })
+  : new IORedis(redisConnection as RedisOptions);
