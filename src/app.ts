cd32c592965c6b5d876d35ddd32ae677ad1beee8
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-06-25 19:05:29 +0200

    lower request size limit

--- src/app.ts
+++ src/app.ts
@@ -41,8 +41,8 @@ if (
     });
 }
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.use("/auth", authRoutes);
 app.use("/api/history", historyRoutes);
