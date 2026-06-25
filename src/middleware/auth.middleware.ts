700c9f01f87917f7728128cbf43c29c5f52661eb
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-06-25 19:11:52 +0200

    remove log

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -23,7 +23,6 @@ export const checkAuth = async (
   next: NextFunction,
 ) => {
   if (process.env.NODE_ENV === "development") {
-    console.log("DEV");
     req.user = { id: "1234" };
     return next();
   }
