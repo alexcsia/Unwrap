ffd2ab4d46a778c27c04c245059d162f979c92e2
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-06-28 22:52:25 +0200

    add deleteQueue

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -3,11 +3,16 @@ import IORedis from "ioredis";
 
 const REDIS_URL = process.env.REDIS_URL!;
 
+console.log("REDIS URL", REDIS_URL);
 export const redisConnection = new IORedis(REDIS_URL, {
   maxRetriesPerRequest: null,
 });
 
 export const historyQueue = new Queue("history-sync", {
+  connection: redisConnection,
+});
+
+export const deleteQueue = new Queue("delete-user", {
   connection: redisConnection,
 });
 
