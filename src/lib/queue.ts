fe78233bbcc1ddfa6ce7b38fcc65a77e57fe1a5f
Alexia Bertalan <alexia.bertalan@code.berlin>
2026-07-12 16:59:13 +0200

    create baseConfig object for all redis connections

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -3,8 +3,17 @@ import IORedis from "ioredis";
 
 const REDIS_URL = process.env.REDIS_URL!;
 
-console.log("REDIS URL", REDIS_URL);
+const baseConfig = {
+  connectTimeout: 5000, // fail if can't connect within 5s
+  lazyConnect: true,
+  retryStrategy: (times: number) => {
+    if (times > 5) return null; // stop retrying after 5 attempts
+    return Math.min(times * 200, 2000); // exponential backoff up to 2s
+  },
+};
+
 export const redisConnection = new IORedis(REDIS_URL, {
+  ...baseConfig,
   maxRetriesPerRequest: null,
 });
 
@@ -16,4 +25,7 @@ export const deleteQueue = new Queue("delete-user", {
   connection: redisConnection,
 });
 
-export const redisCache = new IORedis(REDIS_URL);
+export const redisCache = new IORedis(REDIS_URL, {
+  ...baseConfig,
+  commandTimeout: 3000,
+});
